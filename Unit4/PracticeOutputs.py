ff8f5c82e66665a06f2d2f6219292468a7b16dbe
bettyp23 <bp23e@fsu.edu>
2024-10-15 15:12:16 -0400

    Update PracticeOutputs.py

--- Unit4/PracticeOutputs.py
+++ Unit4/PracticeOutputs.py
@@ -1,2 +1,22 @@
-my_dict = {1: 'May'}
-print(my_dict[0])
+
+
+"""
+SortedSquares
+"""
+
+def sortedSquares(self, nums: list[int]) -> list[int]:
+    left = 0
+    right = len(nums) - 1
+    result = []
+
+    while left <= right:
+        if abs(nums[left]) > abs(nums[right]):
+            reult.append(nums[left] ** 2)
+            left += 1
+        else:
+            result.append(nums[right] ** 2)
+            right -= 1
+
+    result.reverse()
+    return result
+
